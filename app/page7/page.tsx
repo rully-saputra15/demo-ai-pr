f2ed82b873e0dabb70e9d40fbecb8bca720833d6
rullysaputra <rully.saputra@tiket.com>
2026-05-08 20:27:28 +0700

    chore: add new page 7

---
+++ app/page7/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+export default function Page7() {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log("Halo");
+  });
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <button onClick={() => setCounter((prev) => prev++)}>Increment</button>
+    </div>
+  );
+}
